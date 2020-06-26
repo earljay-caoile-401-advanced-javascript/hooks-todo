cebe7f25ff9d1d706ef49984240780245438bf94
Earl Jay Caoile <unfie555@gmail.com>
2020-06-26 14:48:05 -0700

    finished jsdoc comments

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/base.scss';
 
+/**
+ * main component that holds the 3 main components of the app: Header, ToDo, and Footer
+ *
+ * @component
+ * @example
+ * return (
+ *   <App />
+ * )
+ */
 function App() {
   return (
     <BrowserRouter>
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,6 +4,16 @@ import ToDoList from './ToDoList';
 import { Route } from 'react-router-dom';
 import '../styles/todo.scss';
 
+/**
+ * component that renders the TodoForm or ToDoList components based on route
+ * child component of the main App component
+ *
+ * @component
+ * @example
+ * return (
+ *   <ToDo />
+ * )
+ */
 function ToDo() {
   const tasks = [];
 
